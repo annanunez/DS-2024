2e72af4dfb4e04fd0e48f324490bd9710ae2461a
Anna Luiza <165062707+annanunez@users.noreply.github.com>
2024-04-11 15:08:56 -0300

    Delete _GITHUB directory

--- _GITHUB/Lógica de programação e algoritmos/Phyton/10 - Conversão de medidas.py
+++
@@ -1,5 +0,0 @@
-distancia = float(input('Conversão de distância metros para centímetros: '))
-print('Digite o valor em metros para ser convertido: ')
-
-resultado = distancia * 100
-print('O valor convertido é: {:.2f}'.format(resultado))
--- _GITHUB/Lógica de programação e algoritmos/Phyton/4 - porcentagem.py
+++
@@ -1,5 +0,0 @@
-numero = int(input('Digite um número de 1 a 10000: '))
-porcetagem = int(input('Digite a porcentagem: '))
-resultado = numero* (porcetagem / 100)
-
-print("O valor da porcentagem é de: ", resultado)
